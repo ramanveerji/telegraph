41fd800ff1dd88595d1b57a48a88061a96cdbdba
Aditya <xditya@ultroid.tech>
2022-09-04 11:22:12 +0530

    use graph.org inplace of telegra.ph

--- telegraph/api.py
+++ telegraph/api.py
@@ -8,41 +8,40 @@ from .utils import html_to_nodes, nodes_to_html, FilesOpener, json_dumps
 
 
 class TelegraphApi:
-    """ Telegraph API Client
+    """Telegraph API Client
 
     :param access_token: access_token
     :type access_token: str
     """
 
-    __slots__ = ('access_token', 'session')
+    __slots__ = ("access_token", "session")
 
     def __init__(self, access_token=None):
         self.access_token = access_token
         self.session = requests.Session()
 
-    def method(self, method, values=None, path=''):
+    def method(self, method, values=None, path=""):
         values = values.copy() if values is not None else {}
 
-        if 'access_token' not in values and self.access_token:
-            values['access_token'] = self.access_token
+        if "access_token" not in values and self.access_token:
+            values["access_token"] = self.access_token
 
         response = self.session.post(
-            'https://api.telegra.ph/{}/{}'.format(method, path),
-            data=values
+            "https://api.graph.org/{}/{}".format(method, path), data=values
         ).json()
 
-        if response.get('ok'):
-            return response['result']
+        if response.get("ok"):
+            return response["result"]
 
-        error = response.get('error')
-        if isinstance(error, str) and error.startswith('FLOOD_WAIT_'):
-            retry_after = int(error.rsplit('_', 1)[-1])
+        error = response.get("error")
+        if isinstance(error, str) and error.startswith("FLOOD_WAIT_"):
+            retry_after = int(error.rsplit("_", 1)[-1])
             raise RetryAfterError(retry_after)
         else:
             raise TelegraphException(error)
 
     def upload_file(self, f):
-        """ Upload file. NOT PART OF OFFICIAL API, USE AT YOUR OWN RISK
+        """Upload file. NOT PART OF OFFICIAL API, USE AT YOUR OWN RISK
             Returns a list of dicts with `src` key.
             Allowed only .jpg, .jpeg, .png, .gif and .mp4 files.
 
@@ -50,19 +49,16 @@ class TelegraphApi:
         :type f: file, str or list
         """
         with FilesOpener(f) as files:
-            response = self.session.post(
-                'https://telegra.ph/upload',
-                files=files
-            ).json()
+            response = self.session.post("https://graph.org/upload", files=files).json()
 
         if isinstance(response, list):
-            error = response[0].get('error')
+            error = response[0].get("error")
         else:
-            error = response.get('error')
+            error = response.get("error")
 
         if error:
-            if isinstance(error, str) and error.startswith('FLOOD_WAIT_'):
-                retry_after = int(error.rsplit('_',1)[-1])
+            if isinstance(error, str) and error.startswith("FLOOD_WAIT_"):
+                retry_after = int(error.rsplit("_", 1)[-1])
                 raise RetryAfterError(retry_after)
             else:
                 raise TelegraphException(error)
@@ -71,12 +67,12 @@ class TelegraphApi:
 
 
 class Telegraph:
-    """ Telegraph API client helper
+    """Telegraph API client helper
 
     :param access_token: Telegraph access token
     """
 
-    __slots__ = ('_telegraph',)
+    __slots__ = ("_telegraph",)
 
     def __init__(self, access_token=None):
         self._telegraph = TelegraphApi(access_token)
@@ -85,83 +81,96 @@ class Telegraph:
         """Get current access_token"""
         return self._telegraph.access_token
 
-    def create_account(self, short_name, author_name=None, author_url=None,
-                       replace_token=True):
-        """ Create a new Telegraph account
+    def create_account(
+        self, short_name, author_name=None, author_url=None, replace_token=True
+    ):
+        """Create a new Telegraph account
 
         :param short_name: Account name, helps users with several
                            accounts remember which they are currently using.
                            Displayed to the user above the "Edit/Publish"
-                           button on Telegra.ph, other users don't see this name
+                           button on graph.org, other users don't see this name
         :param author_name: Default author name used when creating new articles
         :param author_url: Default profile link, opened when users click on the
                            author's name below the title. Can be any link,
                            not necessarily to a Telegram profile or channels
         :param replace_token: Replaces current token to a new user's token
         """
-        response = self._telegraph.method('createAccount', values={
-            'short_name': short_name,
-            'author_name': author_name,
-            'author_url': author_url
-        })
+        response = self._telegraph.method(
+            "createAccount",
+            values={
+                "short_name": short_name,
+                "author_name": author_name,
+                "author_url": author_url,
+            },
+        )
 
         if replace_token:
-            self._telegraph.access_token = response.get('access_token')
+            self._telegraph.access_token = response.get("access_token")
 
         return response
 
-    def edit_account_info(self, short_name=None, author_name=None,
-                          author_url=None):
-        """ Update information about a Telegraph account.
+    def edit_account_info(self, short_name=None, author_name=None, author_url=None):
+        """Update information about a Telegraph account.
             Pass only the parameters that you want to edit
 
         :param short_name: Account name, helps users with several
                            accounts remember which they are currently using.
                            Displayed to the user above the "Edit/Publish"
-                           button on Telegra.ph, other users don't see this name
+                           button on graph.org, other users don't see this name
         :param author_name: Default author name used when creating new articles
         :param author_url: Default profile link, opened when users click on the
                            author's name below the title. Can be any link,
                            not necessarily to a Telegram profile or channels
         """
-        return self._telegraph.method('editAccountInfo', values={
-            'short_name': short_name,
-            'author_name': author_name,
-            'author_url': author_url
-        })
+        return self._telegraph.method(
+            "editAccountInfo",
+            values={
+                "short_name": short_name,
+                "author_name": author_name,
+                "author_url": author_url,
+            },
+        )
 
     def revoke_access_token(self):
-        """ Revoke access_token and generate a new one, for example,
-            if the user would like to reset all connected sessions, or
-            you have reasons to believe the token was compromised.
-            On success, returns dict with new access_token and auth_url fields
+        """Revoke access_token and generate a new one, for example,
+        if the user would like to reset all connected sessions, or
+        you have reasons to believe the token was compromised.
+        On success, returns dict with new access_token and auth_url fields
         """
-        response = self._telegraph.method('revokeAccessToken')
+        response = self._telegraph.method("revokeAccessToken")
 
-        self._telegraph.access_token = response.get('access_token')
+        self._telegraph.access_token = response.get("access_token")
 
         return response
 
     def get_page(self, path, return_content=True, return_html=True):
-        """ Get a Telegraph page
+        """Get a Telegraph page
 
         :param path: Path to the Telegraph page (in the format Title-12-31,
-                     i.e. everything that comes after https://telegra.ph/)
+                     i.e. everything that comes after https://graph.org/)
         :param return_content: If true, content field will be returned
         :param return_html: If true, returns HTML instead of Nodes list
         """
-        response = self._telegraph.method('getPage', path=path, values={
-            'return_content': return_content
-        })
+        response = self._telegraph.method(
+            "getPage", path=path, values={"return_content": return_content}
+        )
 
         if return_content and return_html:
-            response['content'] = nodes_to_html(response['content'])
+            response["content"] = nodes_to_html(response["content"])
 
         return response
 
-    def create_page(self, title, content=None, html_content=None,
-                    author_name=None, author_url=None, return_content=False):
-        """ Create a new Telegraph page
+    def create_page(
+        self,
+        title,
+        content=None,
+        html_content=None,
+        author_name=None,
+        author_url=None,
+        return_content=False,
+    ):
+        """Create a new Telegraph page
 
         :param title: Page title
         :param content: Content in nodes list format (see doc)
@@ -176,17 +185,28 @@ class Telegraph:
 
         content_json = json_dumps(content)
 
-        return self._telegraph.method('createPage', values={
-            'title': title,
-            'author_name': author_name,
-            'author_url': author_url,
-            'content': content_json,
-            'return_content': return_content
-        })
+        return self._telegraph.method(
+            "createPage",
+            values={
+                "title": title,
+                "author_name": author_name,
+                "author_url": author_url,
+                "content": content_json,
+                "return_content": return_content,
+            },
+        )
 
-    def edit_page(self, path, title, content=None, html_content=None,
-                  author_name=None, author_url=None, return_content=False):
-        """ Edit an existing Telegraph page
+    def edit_page(
+        self,
+        path,
+        title,
+        content=None,
+        html_content=None,
+        author_name=None,
+        author_url=None,
+        return_content=False,
+    ):
+        """Edit an existing Telegraph page
 
         :param path: Path to the page
         :param title: Page title
@@ -202,28 +222,32 @@ class Telegraph:
 
         content_json = json_dumps(content)
 
-        return self._telegraph.method('editPage', path=path, values={
-            'title': title,
-            'author_name': author_name,
-            'author_url': author_url,
-            'content': content_json,
-            'return_content': return_content
-        })
+        return self._telegraph.method(
+            "editPage",
+            path=path,
+            values={
+                "title": title,
+                "author_name": author_name,
+                "author_url": author_url,
+                "content": content_json,
+                "return_content": return_content,
+            },
+        )
 
     def get_account_info(self, fields=None):
-        """ Get information about a Telegraph account
+        """Get information about a Telegraph account
 
         :param fields: List of account fields to return. Available fields:
                        short_name, author_name, author_url, auth_url, page_count
 
                        Default: [“short_name”,“author_name”,“author_url”]
         """
-        return self._telegraph.method('getAccountInfo', {
-            'fields': json_dumps(fields) if fields else None
-        })
+        return self._telegraph.method(
+            "getAccountInfo", {"fields": json_dumps(fields) if fields else None}
+        )
 
     def get_page_list(self, offset=0, limit=50):
-        """ Get a list of pages belonging to a Telegraph account
+        """Get a list of pages belonging to a Telegraph account
             sorted by most recently created pages first
 
         :param offset: Sequential number of the first page to be returned
@@ -231,13 +255,10 @@ class Telegraph:
         :param limit: Limits the number of pages to be retrieved
                       (0-200, default = 50)
         """
-        return self._telegraph.method('getPageList', {
-            'offset': offset,
-            'limit': limit
-        })
+        return self._telegraph.method("getPageList", {"offset": offset, "limit": limit})
 
     def get_views(self, path, year=None, month=None, day=None, hour=None):
-        """ Get the number of views for a Telegraph article
+        """Get the number of views for a Telegraph article
 
         :param path: Path to the Telegraph page
         :param year: Required if month is passed. If passed, the number of
@@ -249,15 +270,14 @@ class Telegraph:
         :param hour: If passed, the number of page views for
                      the requested hour will be returned
         """
-        return self._telegraph.method('getViews', path=path, values={
-            'year': year,
-            'month': month,
-            'day': day,
-            'hour': hour
-        })
+        return self._telegraph.method(
+            "getViews",
+            path=path,
+            values={"year": year, "month": month, "day": day, "hour": hour},
+        )
 
     def upload_file(self, f):
-        """ Upload file. NOT PART OF OFFICIAL API, USE AT YOUR OWN RISK
+        """Upload file. NOT PART OF OFFICIAL API, USE AT YOUR OWN RISK
             Returns a list of dicts with `src` key.
             Allowed only .jpg, .jpeg, .png, .gif and .mp4 files.
 
--- tests/test_html_converter.py
+++ tests/test_html_converter.py
@@ -5,25 +5,34 @@ from telegraph.utils import html_to_nodes, nodes_to_html
 
 HTML_TEST_STR = """
 <p>Hello, world!<br/></p>
-<p><a href="https://telegra.ph/">Test link&lt;/a&gt;</a></p>
+<p><a href="https://graph.org/">Test link&lt;/a&gt;</a></p>
 <figure>
 <img src="/file/6c2ecfdfd6881d37913fa.png"/>
 <figcaption></figcaption>
 </figure>
-""".replace('\n', '')
+""".replace(
+    "\n", ""
+)
 
 NODES_TEST_LIST = [
-    {'tag': 'p', 'children': ['Hello, world!', {'tag': 'br'}]},
-    {'tag': 'p', 'children': [{
-        'tag': 'a',
-        'attrs': {'href': 'https://telegra.ph/'},
-        'children': ['Test link</a>']
-        }]
-     },
-    {'tag': 'figure', 'children': [
-        {'tag': 'img', 'attrs': {'src': '/file/6c2ecfdfd6881d37913fa.png'}},
-        {'tag': 'figcaption'}
-    ]}
+    {"tag": "p", "children": ["Hello, world!", {"tag": "br"}]},
+    {
+        "tag": "p",
+        "children": [
+            {
+                "tag": "a",
+                "attrs": {"href": "https://graph.org/"},
+                "children": ["Test link</a>"],
+            }
+        ],
+    },
+    {
+        "tag": "figure",
+        "children": [
+            {"tag": "img", "attrs": {"src": "/file/6c2ecfdfd6881d37913fa.png"}},
+            {"tag": "figcaption"},
+        ],
+    },
 ]
 
 HTML_MULTI_LINES = """
@@ -39,10 +48,7 @@ HTML_MULTI_LINES = """
 HTML_MULTI_LINES1 = """<p><b>Hello, </b>world! </p>"""
 
 HTML_MULTI_LINES_NODES_LIST = [
-    {'tag': 'p', 'children': [
-        {'tag': 'b', 'children': ['Hello, ']},
-        'world! '
-    ]},
+    {"tag": "p", "children": [{"tag": "b", "children": ["Hello, "]}, "world! "]},
 ]
 
 HTML_NO_STARTTAG = "</a><h1></h1>"
@@ -50,36 +56,23 @@ HTML_NO_STARTTAG = "</a><h1></h1>"
 
 class TestHTMLConverter(TestCase):
     def test_html_to_nodes(self):
-        self.assertEqual(
-            html_to_nodes(HTML_TEST_STR),
-            NODES_TEST_LIST
-        )
+        self.assertEqual(html_to_nodes(HTML_TEST_STR), NODES_TEST_LIST)
 
     def test_nodes_to_html(self):
-        self.assertEqual(
-            nodes_to_html(NODES_TEST_LIST),
-            HTML_TEST_STR
-        )
+        self.assertEqual(nodes_to_html(NODES_TEST_LIST), HTML_TEST_STR)
 
     def test_html_to_nodes_multi_line(self):
-        self.assertEqual(
-            html_to_nodes(HTML_MULTI_LINES),
-            HTML_MULTI_LINES_NODES_LIST
-        )
-        self.assertEqual(
-            html_to_nodes(HTML_MULTI_LINES1),
-            HTML_MULTI_LINES_NODES_LIST
-        )
+        self.assertEqual(html_to_nodes(HTML_MULTI_LINES), HTML_MULTI_LINES_NODES_LIST)
+        self.assertEqual(html_to_nodes(HTML_MULTI_LINES1), HTML_MULTI_LINES_NODES_LIST)
 
     def test_uppercase_tags(self):
         self.assertEqual(
-            html_to_nodes("<P>Hello</P>"),
-            [{'tag': 'p', 'children': ['Hello']}]
+            html_to_nodes("<P>Hello</P>"), [{"tag": "p", "children": ["Hello"]}]
         )
 
     def test_html_to_nodes_invalid_html(self):
         with self.assertRaises(InvalidHTML):
-            html_to_nodes('<p><b></p></b>')
+            html_to_nodes("<p><b></p></b>")
 
     def test_html_to_nodes_not_allowed_tag(self):
         with self.assertRaises(NotAllowedTag):
@@ -87,57 +80,69 @@ class TestHTMLConverter(TestCase):
 
     def test_nodes_to_html_nested(self):
         self.assertEqual(
-            nodes_to_html([
-                {'tag': 'a', 'children': [
-                    {'tag': 'b', 'children': [
-                        {'tag': 'c', 'children': [
-                            {'tag': 'd', 'children': []}
-                        ]}
-                    ]}
-                ]}
-            ]),
-            '<a><b><c><d></d></c></b></a>'
+            nodes_to_html(
+                [
+                    {
+                        "tag": "a",
+                        "children": [
+                            {
+                                "tag": "b",
+                                "children": [
+                                    {
+                                        "tag": "c",
+                                        "children": [{"tag": "d", "children": []}],
+                                    }
+                                ],
+                            }
+                        ],
+                    }
+                ]
+            ),
+            "<a><b><c><d></d></c></b></a>",
         )
 
     def test_nodes_to_html_blank(self):
-        self.assertEqual(
-            nodes_to_html([]),
-            ''
-        )
+        self.assertEqual(nodes_to_html([]), "")
 
     def test_clear_whitespace(self):
         i = (
-            '\n<p><i>A</i><b> </b><b>B <i>C</i><i><b></b></i>'
-            ' D </b> E </p><p> F </p>\n'
+            "\n<p><i>A</i><b> </b><b>B <i>C</i><i><b></b></i>"
+            " D </b> E </p><p> F </p>\n"
         )
         expected = [
-            {'tag': 'p', 'children': [
-                {'tag': 'i', 'children': ['A']},
-                {'tag': 'b', 'children': [' ']},
-                {'tag': 'b', 'children': [
-                    'B ',
-                    {'tag': 'i', 'children': ['C']},
-                    {'tag': 'i', 'children': [{'tag': 'b'}]},
-                    ' D '
-                ]},
-                'E '
-            ]},
-            {'tag': 'p', 'children': ['F ']}
+            {
+                "tag": "p",
+                "children": [
+                    {"tag": "i", "children": ["A"]},
+                    {"tag": "b", "children": [" "]},
+                    {
+                        "tag": "b",
+                        "children": [
+                            "B ",
+                            {"tag": "i", "children": ["C"]},
+                            {"tag": "i", "children": [{"tag": "b"}]},
+                            " D ",
+                        ],
+                    },
+                    "E ",
+                ],
+            },
+            {"tag": "p", "children": ["F "]},
         ]
 
         self.assertEqual(html_to_nodes(i), expected)
 
     def test_clear_whitespace_1(self):
-        x = '\n<p><i>A</i><b> </b><b>B <i>C</i><i><b></b></i> D </b> E </p><p> F </p>\n'
-        y = '<p><i>A</i><b> </b><b>B <i>C</i><i><b></b></i> D </b>E </p><p>F </p>'
+        x = "\n<p><i>A</i><b> </b><b>B <i>C</i><i><b></b></i> D </b> E </p><p> F </p>\n"
+        y = "<p><i>A</i><b> </b><b>B <i>C</i><i><b></b></i> D </b>E </p><p>F </p>"
         self.assertEqual(nodes_to_html(html_to_nodes(x)), y)
 
     def test_pre_whitespace_preserved(self):
         self.assertEqual(
             html_to_nodes("<pre>\nhello\nworld</pre>"),
-            [{'tag': 'pre', 'children': ['\nhello\nworld']}]
+            [{"tag": "pre", "children": ["\nhello\nworld"]}],
         )
 
     def test_no_starttag_node(self):
         with self.assertRaises(InvalidHTML):
-             html_to_nodes(HTML_NO_STARTTAG)
+            html_to_nodes(HTML_NO_STARTTAG)
